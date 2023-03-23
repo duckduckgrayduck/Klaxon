7bc7cb3cb89b2c7626d0465518c4280f0e2560be
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-03-22 19:54:02 -0500

    Added functionality to pull the last recent archive.org entry for a page and get the resulting html.

--- main.py
+++ main.py
@@ -9,16 +9,29 @@ import savepagenow
 
 class Klaxon(AddOn):
     """Add-On that will monitor a site for changes and alert you for updates"""
-
+    
+    def monitor_with_selector(site, selector):
+        
+    
+    def full_page_monitor(site):
+        params = {'url':site}
+        response = requests.get('http://web.archive.org/cdx/search/cdx', params=params)
+        successul_saves = []
+        for line in response.text.splitlines():
+            if ' 200 ' in line:
+                successful_saves.append(line)
+        last_save = successful_saves[-1]
+        timestamp = last_save.split('/')[1].split('h')[0].strip()
+        # Now that we have the timestamp for the last successful wayback entry, we can pull the HTML
+        response = requests.get(f'https://web.archive.org/web/{timestamp}/{site}')
+    
     def main(self):
         site = self.data.get("site")
-        project = self.data["project"]
-        # if project is an integer, use it as a project ID
-        try:
-            self.project = int(project)
-        except ValueError:
-            project, created = self.client.projects.get_or_create_by_title(project)
-            self.project = project.id
+        selector = self.data.get("selector")
+        if selector is None:
+            monitor_with_selector(site, selector)
+        else:
+            full_page_monitor(site)
         
         self.send_mail("Hello World!", "We finished!")
 
