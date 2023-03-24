e16238ebf1566046f04a9bb8389a79926780430c
mitchelljkotler <mitch@muckrock.com>
2023-03-24 09:46:19 -0400

    Update main.py
    This looks really good so far.  I know you still have error checking to do, so did not comment on that, but added some other suggestions.

--- main.py
+++ main.py
@@ -16,20 +16,57 @@ import savepagenow
 
 class Klaxon(AddOn):
     """Add-On that will monitor a site for changes and alert you for updates"""
+    
+    #
+    # This function is getting a little too large and dense - I think starting to
+    # refactor it into multiple functions where appropriate would be good, as well
+    # as just putting some new lines in places to break the code up into some sort
+    # of logical "paragraphs" would make it more readable
+    #
+    
     def monitor_with_selector(self, site, selector):
         # Get the full list of archive.org entries
         response = requests.get(f'http://web.archive.org/cdx/search/cdx?url={site}')
         # Filter only for the successful entries
         successful_saves = []
         for line in response.text.splitlines():
+            
+            #
+            # I am slightly worried about just searching for " 200 " somehow returning
+            # a false positive if the format changes.  Maybe
+            # line.split()[4] == "200"
+            # would be more robust?  Not certain on this one.
+            # Also, this for loop could be a list comprehension:
+            #
+            # succesful_saves = [
+            #    line for line in response.text.splitlines()
+            #    if line.split()[4] == "200"
+            # ]
+            #
+            # I generally prefer list comprehensions, although if it is complex enough
+            # a for loop may be easier to understand
+            #
+            
             if ' 200 ' in line:
                 successful_saves.append(line)
         # Get the last successful entry & timestamp for that entry
         last_save = successful_saves[-1]
+        
+        # 
+        # I believe this would be better written as:
+        # r = re.search("\d{14}", last_save)
+        #
+        
         r = re.search("\d\d\d\d\d\d\d\d\d\d\d\d\d\d", last_save)
         timestamp = r.group()
         # Generate the URL for the last successful save's raw HTML file
         last_save_url = f'https://web.archive.org/web/{timestamp}id_/{site}'
+        
+        #
+        # Going from url -> elements (lines 69 - 73 and 74 - 77) would
+        # be a good candidate for factoring out into its own function
+        #
+        
         # Now that we have the timestamp for the last successful wayback entry, we can pull the HTML
         last_save_html = requests.get(last_save_url)
         # And pass it to BeautifulSoup to view only the css selectors we care about.
@@ -44,6 +81,13 @@ class Klaxon(AddOn):
             self.set_message("No changes in page since last archive")
             sys.exit(0)
         else:
+            
+            #
+            # Another one where I think list comprehensions would be clearer:
+            # old_tags = [x.prettify() for x in old_elements]
+            # and similar for new_tags
+            #
+            
             # Generating a list of strings using prettify to pass to difflib
             old_tags = []
             for x in old_elements:
